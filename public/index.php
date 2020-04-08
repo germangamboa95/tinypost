3df10417c1fa7e4eaae6b91e67e04dcf3afacfb8
germangamoa95 <germangamboa95@gmail.com>
2020-04-08 01:44:05 -0400

    readme added

--- public/index.php
+++ public/index.php
@@ -4,8 +4,9 @@ use TinyWeb\Http\Routing\Request;
 
 $app = require_once(__DIR__ . "/../framework/bootstrap.php");
 
-
 $router = $app->get('router');
 
-
 $response = $router->handle(Request::make());
+
+// Just die
+die();
