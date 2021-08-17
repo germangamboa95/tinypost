1670bab2bfb04f54a7c9d50240f56411f0ed8dcf
germangamboa95 <germangamboa95@gmail.com>
2021-08-17 00:01:18 -0400

    add build to gitignore

--- src/api.ts
+++ src/api.ts
@@ -1,27 +1,21 @@
-import express from "express"
+import express from 'express';
 import { Server } from '@overnightjs/core';
-import {controllers} from "./controllers"
-
+import { controllers } from './controllers';
 
 export class ApiServer extends Server {
-    
-    constructor() {
-        super(process.env.NODE_ENV === 'development'); // setting showLogs to true
-        this.app.use(express.json());
-        this.setupControllers();
-    }
+  constructor() {
+    super(process.env.NODE_ENV === 'development'); // setting showLogs to true
+    this.app.use(express.json());
+    this.setupControllers();
+  }
 
-    private setupControllers(): void {
+  private setupControllers(): void {
+    super.addControllers(controllers.map((c) => new c()));
+  }
 
- 
-        super.addControllers(
-            controllers.map(c => new c())
-        );
-    }
-
-    public start(port: number): void {
-        this.app.listen(port, () => {
-            console.log('Server listening on port: ' + port);
-        })
-    }
+  public start(port: number): void {
+    this.app.listen(port, () => {
+      console.log('Server listening on port: ' + port);
+    });
+  }
 }
