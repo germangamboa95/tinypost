588b079cb1da3e8584f6872c38e34cbf9b6e98bc
germangamboa95 <germangamboa95@gmail.com>
2021-08-16 23:27:22 -0400

    restarting project

--- src/Routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use TinyWeb\Http\Routing\Request;
-use TinyWeb\Http\Routing\Router;
-
-/** @var Router */
-$router = $this->get(Router::class);
-
-$router->get('/', 'HomeController@index');
